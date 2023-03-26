1184b266e6d2ff98ae34a646524867955a492fde
Abdul <abdulamite@gmail.com>
2023-03-25 19:28:32 -0700

    introduce flag to only allow for real pull requests

--- README.md
+++ README.md
@@ -25,6 +25,8 @@ The bot will listens for specific messages from users on Discord. These messages
 !pr-all: Lists all the pull requests in the queue.
 ```
 
+There is an optional flag to located in the `index.js` file called `onlyAllowRealPullRequests`. When this flag is set to true, the bot will only allow pull requests that are actually open on the repo to be added to the queue. This is useful if you want to make sure that the pull requests you are adding to the queue are actually valid.
+
 ##Adding your own commands
 You can add your own commands to the bot by creating a new condition in the if/else statement in the following block of code in the index.js file:
 
--- index.js
+++ index.js
@@ -16,62 +16,96 @@ const githubOwner = process.env.GITHUB_OWNER;
 const githubRepo = process.env.GITHUB_REPO;
 const githubToken = process.env.GITHUB_TOKEN;
 
+// This flag is used to make sure that only real pull requests are added to the queue
+// otherwise anyone can add any number to the queue
+const onlyAllowRealPullRequests = true;
+
 // Array to store the pull requests
 let pullRequests = [];
-client.on("messageCreate", async (message) => {
-  if (message.content.includes("!pr-list")) {
-    const response = await axios.get(
-      `${githubApiBaseUrl}/repos/${githubOwner}/${githubRepo}/pulls`,
-      {
-        headers: {
-          Authorization: `token ${githubToken}`,
-        },
-      }
-    );
-    pullRequests = response.data;
-    const prList = pullRequests.map((pr) => `${pr.number} -${pr.title}`);
-    if (prList.length === 0) {
-      message.reply("No PRs found");
-      return;
+let queablePullRequests = [];
+
+client.on("ready", async () => {
+  const response = await axios.get(
+    `${githubApiBaseUrl}/repos/${githubOwner}/${githubRepo}/pulls`,
+    {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
     }
-    message.reply(prList.join("\n"));
-  } else if (message.content.includes("!pr-add:")) {
-    const splitMessage = message.content.split(":");
-    const prNumber = parseInt(splitMessage[1]);
-    const isValidNumber = !isNaN(prNumber) && prNumber > 0;
-    if (!isValidNumber) {
+  );
+  pullRequestsFromRepo = response.data;
+  const prList = pullRequestsFromRepo.map(function (pr) {
+    return pr.number;
+  });
+  if (prList.length === 0) {
+    console.log("No PRs found");
+    return;
+  } else {
+    queablePullRequests = [...prList];
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  const { content } = message;
+  const prListUrl = `${githubApiBaseUrl}/repos/${githubOwner}/${githubRepo}/pulls`;
+
+  if (content.includes("!pr-list")) {
+    const response = await axios.get(prListUrl, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    });
+    const prList = response.data.map((pr) => `${pr.number} - ${pr.title}`);
+    message.reply(prList.length ? prList.join("\n") : "No PRs found");
+  } else if (content.includes("!pr-add:")) {
+    const prNumber = parseInt(content.split(":")[1]);
+
+    if (isNaN(prNumber) || prNumber <= 0) {
       message.reply("Please enter a valid PR number");
       return;
     }
+
+    if (onlyAllowRealPullRequests && !queablePullRequests.includes(prNumber)) {
+      message.reply("Please enter a PR number from the repo");
+      return;
+    }
+
+    if (pullRequests.includes(prNumber)) {
+      message.reply("PR already in the queue");
+      return;
+    }
+
     pullRequests.push(prNumber);
     message.reply(`PR ${prNumber} added to the queue`);
-  } else if (message.content.includes("!pr-help")) {
-    message.reply(`
-    Here are the commands you can use:
-    \`\`\`
-    !pr-list - List all the PRs from the repo
-    !pr-add:<PR_NUMBER> - Add a PR to the queue.
-    !pr-remove - Remove the first PR from the queue
-    !pr-clear - Clear the queue
-    !pr-all - List all the PRs in the queue\`\`\`
+  } else if (content.includes("!pr-help")) {
+    message.reply(`Here are the commands you can use:
+      \`\`\`
+      !pr-list - List all the PRs from the repo
+      !pr-add:<PR_NUMBER> - Add a PR to the queue.
+      !pr-remove - Remove the first PR from the queue
+      !pr-clear - Clear the queue
+      !pr-all - List all the PRs in the queue
+      \`\`\`
     `);
-  } else if (message.content.includes("!pr-remove")) {
+  } else if (content.includes("!pr-remove")) {
+    // Because this is a queue, we remove the first PR from the queue because queues are FIFO
     pullRequests.shift();
     message.reply(
-      `\nFirst PR removed from the queue here is the new queue \n: ${pullRequests.join(
+      `First PR removed from the queue, here is the new queue:\n${pullRequests.join(
         "\n"
       )}`
     );
-  } else if (message.content.includes("!pr-clear")) {
+  } else if (content.includes("!pr-clear")) {
     pullRequests = [];
-    message.reply(`\nQueue cleared`);
-  } else if (message.content.includes("!pr-all")) {
-    if (pullRequests.length === 0) {
-      message.reply("No PRs found");
-      return;
-    } else {
-      message.reply(`\n${pullRequests.join("\n")}`);
-    }
+    message.reply("Queue cleared");
+  } else if (content.includes("!pr-all")) {
+    message.reply(
+      pullRequests.length > 0 ? JSON.stringify(pullRequests) : "No PRs found"
+    );
   }
 });
 
